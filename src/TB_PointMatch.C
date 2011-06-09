2e204778f9fa1fbac5d9a20bc336ea10f5320630
szymon.kapeniak <szymon.kapeniak@cd0760b3-73b7-1dd1-266f-e124e609c0ae>
2011-06-09 17:59:58 +0000

    Removing Spay Tree from TB_PointMatch class, as it was a wrong choice for our purpose. STL map (red/black tree) used instead. It runs faster, and should allow multi-thread access from VEX (?).

--- src/TB_PointMatch.C
+++ src/TB_PointMatch.C
@@ -2,41 +2,25 @@
 
 using namespace TimeBlender;
 
-int
-compareSplayNodes(const void * a, const void * b)
-{
-    if ( ((TB_SplayNode*)a)->id < ((TB_SplayNode*)b)->id )
-    {
-        return -1;
-    }
-    else if ( ((TB_SplayNode *)a)->id > ((TB_SplayNode *)b)->id ) 
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-/// TODO: Profile memory in this!
 int 
-TB_PointMatch::initialize(const GU_Detail * gdp, int correspond = 0)
+TB_PointMatch::initialize(GU_Detail * gdp, int correspond = 0)
 {
     /// Init accelerator:
-    tree = new UT_SplayTree(&compareSplayNodes);
-    GEO_AttributeHandle handle = gdp->getAttribute(GEO_POINT_DICT, "id");
-    const GEO_Point * ppt;
-	TB_SplayNode * snode;
-	
-	/// Create accelerator:
+    tree = new Tree();
+    GEO_AttributeHandle handle = gdp->getPointAttribute("id");
+    std::map<int, GEO_Point*>::iterator it;
+    GEO_Point * ppt;
+    int id;
+    
+	/// Create red/black tree accelerator:
 	FOR_ALL_GPOINTS(gdp, ppt)
 	{
+	    it = tree->end();
 	    handle.setElement(ppt);
-	    snode = new TB_SplayNode((int)handle.getI());
-	    snode->ppt = (const GEO_Point*)ppt;
-	    tree->add(snode);
+	    id = handle.getI();
+	    tree->insert(it, std::pair<int, GEO_Point*>(id, ppt));
 	}
+	
 	detail = gdp;
 	alloc  = true; 
 	return 1;
@@ -44,12 +28,9 @@ TB_PointMatch::initialize(const GU_Detail * gdp, int correspond = 0)
 
 GEO_Point * TB_PointMatch::find(int id)
 {
-    /// g++ complains that we are taking an address of a temporary
-    /// object here, but it shouldn't hurt here imho.
-    TB_SplayNode * match = (TB_SplayNode *) tree->find(&TB_SplayNode(id));
-    if (!match)
+    if (alloc) 
+        return tree->find(id)->second;
+    else
         return NULL;
-    GEO_Point * ppt = (GEO_Point*) match->ppt;
-    return ppt;
 }
 
